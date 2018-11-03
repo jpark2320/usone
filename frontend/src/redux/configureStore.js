ece635689ee656c4ca7d74710f05767f019e3fe3
jpark2320 <jpark2320@gmail.com>
2018-11-03 15:39:23 -0700

    Install redux-thunk and add it to the middelware for redux

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,14 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import user from "redux/modules/user";
+
+const middlewares = [thunk];
 
 const reducer = combineReducers({
   user
 });
 
-let store = initialState => createStore(reducer);
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
