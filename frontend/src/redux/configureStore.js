58618636e053c458db4d09a299308f13eebbf7b6
jpark2320 <jpark2320@gmail.com>
2018-11-03 22:16:44 -0700

    Configure Redux and make user reducer

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createHistory from "history/createBrowserHistory";
 import thunk from "redux-thunk";
 import user from "redux/modules/user";
 
 const env = process.env.NODE_ENV;
-
-const middlewares = [thunk];
+const history = createHistory();
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
   const { logger } = require("redux-logger");
@@ -12,10 +15,19 @@ if (env === "development") {
 }
 
 const reducer = combineReducers({
+  routing: routerReducer,
   user
 });
 
-let store = initialState =>
-  createStore(reducer, applyMiddleware(...middlewares));
+let store;
+
+if (env === "development") {
+  store = initialState =>
+    createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+
+export { history };
 
 export default store();
