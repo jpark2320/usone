ba9379411a35fdd48c5ee15118ea291313ef4d6b
jpark2320 <jpark2320@gmail.com>
2018-11-14 02:09:11 -0800

    Remove createsuperuser command from .ebextensions

--- usone/users/management/commands/ebuser.py
+++ usone/users/management/commands/ebuser.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+# Used this function to create super user
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
         User = get_user_model()
-        User.objects.create_superuser('jpark2320', 'jpark2320@gmail.com', 'usone123')
+        User.objects.create_superuser('jpark2320', 'jpark2320@gmail.com', '1234567890')
