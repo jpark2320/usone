561bf766c0d49eca00f4140d392952a63f1bcd4a
Yunhu Choi <ck43789@gmail.com>
2018-11-19 23:29:48 -0500

    Add route with url

--- frontend/src/components/App/presenter.tsx
+++ frontend/src/components/App/presenter.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import MainView from "views/MainView";
 import { Route, Switch, Redirect } from "react-router-dom";
+import ListView from "views/ListView";
 
 const App: React.SFC = () => <Routes />;
 
 const Routes: React.SFC = () => (
   <Switch>
     <Route path={"/"} exact={true} component={MainView} />
+    <Route path="/sell" component={ListView} />
+    <Route path="/buy" component={ListView} />
+    <Route path="/buy" component={ListView} />
+    <Route path="/house" component={ListView} />
+    <Route path="/job" component={ListView} />
+    <Route path="/visa" component={ListView} />
     <Redirect from={"*"} to={"/"} />
   </Switch>
 );
