125c9a5deaef17abcc0b2f68b6c758a8f761df05
Yunhu Choi <ck43789@gmail.com>
2018-11-28 12:37:15 -0500

    Code clean in ListItemLink

--- frontend/src/components/ListItemLink/container.tsx
+++ frontend/src/components/ListItemLink/container.tsx
@@ -1,12 +1,6 @@
 import * as React from "react";
 import ListItemLink from "./presenter";
 
-const Container = (props: any) => (
-  <ListItemLink
-    primary={props.primary}
-    to={props.to}
-    primaryStyle={props.primaryStyle}
-  />
-);
+const Container = (props: any) => <ListItemLink {...props} />;
 
 export default Container;
--- frontend/src/components/ListItemLink/index.tsx
+++ frontend/src/components/ListItemLink/index.tsx
@@ -1,9 +1,4 @@
 import { connect } from "react-redux";
 import Container from "./container";
 
-const mapStateToProps = state => ({
-  counter: state.counter,
-  router: state.router
-});
-
-export default connect(mapStateToProps)(Container);
+export default connect()(Container);
