5cae1d098616a8b6b9169e0f0261d41ffb732ad3
Yunhu Choi <ck43789@gmail.com>
2018-11-29 14:20:04 -0500

    Set Region API.

--- frontend/src/redux/modules/posts.ts
+++ frontend/src/redux/modules/posts.ts
@@ -2,12 +2,20 @@
 
 // ACTIONS
 const SET_ALL_POSTS = "SET_ALL_POSTS";
+const SET_POSTS_BY_REGION = "SET_POSTS_BY_REGION";
 
 // ACTION CREATORS
 function setAllPosts(allPosts) {
   return {
     type: SET_ALL_POSTS,
     allPosts
+  };
+}
+
+function setPostsByRegion(PostsByRegion) {
+  return { 
+    type: SET_POSTS_BY_REGION, 
+    PostsByRegion 
   };
 }
 
@@ -21,6 +29,15 @@ function getAllPosts() {
   };
 }
 
+function getPostsByRegion(region: string) {
+  return dispatch => {
+    fetch("/posts-by-region/?q = " + region)
+      .then(response => response.json())
+      .then(json => dispatch(setPostsByRegion(json)))
+      .catch(err => console.log(err));
+  };
+}
+
 // INITIAL STATE
 const initialState = {};
 
@@ -29,6 +46,8 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_ALL_POSTS:
       return applySetAllPosts(state, action);
+    case SET_POSTS_BY_REGION:
+      return applySetPostsByRegion(state, action);
     default:
       return state;
   }
@@ -43,9 +62,17 @@ function applySetAllPosts(state, action) {
   };
 }
 
+function applySetPostsByRegion(state, action) {
+  const { allPosts } = action;
+  return {
+    ...state,
+    allPosts
+  };
+}
 // EXPORT
 const actionCreators = {
-  getAllPosts
+  getAllPosts,
+  getPostsByRegion
 };
 export { actionCreators };
 
