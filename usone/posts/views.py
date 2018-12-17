af5b4a558d6cc4983e8580d6c27ef4864965a706
poolpnd <poolpnd@hotmail.com>
2018-12-16 20:19:20 -0500

    t

--- usone/posts/views.py
+++ usone/posts/views.py
@@ -15,7 +15,7 @@ from . import serializers
 
 
 class BasicSizePagination(pagination.PageNumberPagination):
-    page_size = 25
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
